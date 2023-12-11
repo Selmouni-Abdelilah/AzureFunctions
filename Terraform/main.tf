51e4d3a10fc8aeb409c5108f6a1ece8fe5e88c26
Selmouni Abdelilah <123@gmail.com>
2023-12-11 17:58:17 +0100

    update

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -4,16 +4,16 @@ data "azurerm_resource_group" "rg" {
 
 resource "azurerm_storage_account" "storage" {
   name                     = "functiontrigger2023"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = data.azurerm_resource_group.rg.name
+  location                 = data.azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
 resource "azurerm_service_plan" "svcplan" {
   name                = "example-app-service-plan"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
   os_type             = "Linux"
   sku_name            = "Y1"
 }
@@ -38,8 +38,8 @@ resource "azurerm_storage_blob" "blob" {
 
 resource "azurerm_linux_function_app" "function" {
   name                = var.function_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
 
   storage_account_name       = azurerm_storage_account.storage.name
   storage_account_access_key = azurerm_storage_account.storage.primary_access_key
