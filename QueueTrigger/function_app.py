ad00abc438611379bfa904591bfc17760da34dec
Selmouni-Abdelilah <128396251+Selmouni-Abdelilah@users.noreply.github.com>
2023-12-11 23:46:21 +0100

    Update function_app.py

--- QueueTrigger/function_app.py
+++ QueueTrigger/function_app.py
@@ -1,13 +1,10 @@
 import azure.functions as func
 import logging
-import os
 
 app = func.FunctionApp()
 
-queue_connection_string = os.environ["QueueConnectionString"]
-queue_name = os.environ["QueueName"]
-queue_trigger_name = os.environ["QueueTriggerName"]
-
-@app.queue_trigger(arg_name=queue_trigger_name, queue_name=queue_name, connection=queue_connection_string)
+@app.queue_trigger(arg_name="azqueue", queue_name="queuenametrigger",
+                               connection="QueueConnectionString") 
 def queue_trigger(azqueue: func.QueueMessage):
-    logging.info('Python Queue trigger processed a message: %s', azqueue.get_body().decode('utf-8'))
+    logging.info('Python Queue trigger processed a message: %s',
+                azqueue.get_body().decode('utf-8'))
