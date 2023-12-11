ad1446c1ca6ed442ad33fbf353db4d12aaa43d73
Selmouni-Abdelilah <128396251+Selmouni-Abdelilah@users.noreply.github.com>
2023-12-11 23:45:33 +0100

    Update function_app.py

--- BlobTrigger/function_app.py
+++ BlobTrigger/function_app.py
@@ -5,8 +5,8 @@ app = func.FunctionApp()
 
 @app.blob_trigger(arg_name="blobnametrigger", path="content",
                                connection="BlobStorageConnectionString") 
-def blob_trigger(myblob: func.InputStream):
+def blob_trigger(blobnametrigger: func.InputStream):
     logging.info(f"Python blob trigger function processed blob"
-                f"Name: {myblob.name}"
-                f"Blob Size: {myblob.length} bytes")
+                f"Name: {blobnametrigger.name}"
+                f"Blob Size: {blobnametrigger.length} bytes")
 
