51e00a5fc6bdbb1d2364f3d76d35813cfe46ae23
Selmouni-Abdelilah <128396251+Selmouni-Abdelilah@users.noreply.github.com>
2023-12-11 23:43:18 +0100

    Update function_app.py

--- BlobTrigger/function_app.py
+++ BlobTrigger/function_app.py
@@ -1,15 +1,12 @@
 import azure.functions as func
 import logging
-import os
 
 app = func.FunctionApp()
 
-blob_connection_string = os.environ["BlobStorageConnectionString"]
-blob_container_name = os.environ["BlobContainerName"]
-blob_trigger_name = os.environ["BlobTriggerName"]
-
-@app.blob_trigger(arg_name=blob_trigger_name, path=blob_container_name, connection=blob_connection_string)
+@app.blob_trigger(arg_name="blobnametrigger", path="content",
+                               connection="BlobStorageConnectionString") 
 def blob_trigger(myblob: func.InputStream):
     logging.info(f"Python blob trigger function processed blob"
                 f"Name: {myblob.name}"
-                f"Blob Size: {myblob.length}")
+                f"Blob Size: {myblob.length} bytes")
+
