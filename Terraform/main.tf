fd6cd23fe35a78e6fe0867e453f23a230e8fce67
Selmouni-Abdelilah <128396251+Selmouni-Abdelilah@users.noreply.github.com>
2023-12-11 19:08:32 +0100

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -3,7 +3,7 @@ data "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "Timertrigger2023"
+  name                     = "timertrigger2023"
   resource_group_name      = data.azurerm_resource_group.rg.name
   location                 = data.azurerm_resource_group.rg.location
   account_tier             = "Standard"
